60c57869d059fb7a9f58171b88e8341a9b49da76
Igor39-dev <19gorokhov90@gmail.com>
2026-05-01 23:33:39 +0200

    async GET query via sqlalchemy

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import Query, Body, APIRouter, Depends
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
 from src.models.hotels import HotelsOrm
@@ -11,38 +11,23 @@ from src.database import engine
 router = APIRouter(prefix="/hotels", tags=["ОТЕЛИ"])
 
 
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубау", "name": "dubai"},
-    {"id": 3, "title": "Москва", "name": "moscow"}, 
-    {"id": 4, "title": "Санкт-Петербург", "name": "spb"},
-    {"id": 5, "title": "Екатеринбург", "name": "ekb"},
-    {"id": 6, "title": "Новосибирск", "name": "nsk"},
-    {"id": 7, "title": "Красноярск", "name": "krasnoyarsk"},
-    {"id": 8, "title": "Хабаровск", "name": "habarovsk"},
-    {"id": 9, "title": "Владивосток", "name": "vladivostok"},
-    {"id": 10, "title": "Магадан", "name": "magadan"},
-    {"id": 11, "title": "Иркутск", "name": "irkutsk"},
-]
-
 
 @router.get("")
-def get_hotels(
+async def get_hotels(
     pagination: PaginationDep,
     id: int | None = Query(default=None, description="ID отеля"),
     title: str | None = Query(default=None, description="Название отеля"),
 ):
-    hotels_ = []
-    for hotel in hotels:
-        if id and hotel["id"] != id:
-            continue
-        if title and hotel["title"] != title:
-            continue
-        hotels_.append(hotel)
+    async with async_session_maker() as session:
+        query = select(HotelsOrm)
+        result =await session.execute(query)
+        # print(result.scalars().all())
+        hotels = result.scalars().all()
+        return hotels
 
-    if pagination.page and pagination.per_page:
-        return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
-    return hotels_
+    # if pagination.page and pagination.per_page:
+    #     return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
+
 
 @router.post("")
 async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
