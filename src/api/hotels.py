5099b1dff5865cf0fae9eb99fa7ca1296d660a76
Igor39-dev <19gorokhov90@gmail.com>
2026-05-01 23:01:01 +0200

    update post
    async add objects to DB by sqlalchemy

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 from fastapi import Query, Body, APIRouter, Depends
+from sqlalchemy import insert
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+from src.models.hotels import HotelsOrm
 from src.shemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix="/hotels", tags=["ОТЕЛИ"])
@@ -40,13 +43,26 @@ def get_hotels(
     return hotels_
 
 @router.post("")
-def create_hotel(hotel: Hotel):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": hotel.title,
-        "name": hotel.name,
-    })
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+    "1": {
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель Сочи 5 звезда у моря",
+            "location": "ул. Катина, 1"
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель Дубай 1 звезда",
+            "location": "ул. Шейха, 2"
+        }
+    }
+})):
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
     return {"status": "OK"}
 
 
--- src/shemas/hotels.py
+++ src/shemas/hotels.py
@@ -3,9 +3,10 @@ from pydantic import BaseModel, ConfigDict
 
 class Hotel(BaseModel):
     title: str
-    name: str
+    location: str
 
 
 class HotelPATCH(BaseModel):
     title: str | None = None
-    name: str | None = None
+    location: str | None = None
+    
