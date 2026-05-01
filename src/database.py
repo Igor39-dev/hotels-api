ee71db5a7f78a5539700504aaf0775df6d9fa726
Igor39-dev <19gorokhov90@gmail.com>
2026-05-01 19:57:26 +0200

    create model + migrations
    - Hotel model (HotelOrm)
    - migrations (alembic):
      alembic.ini, migrations/env.py

--- src/database.py
+++ src/database.py
@@ -1,14 +1,14 @@
-import asyncio
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 
 
 engine = create_async_engine(settings.DB_URL)
 
-async def func():
-    async with engine.begin() as conn:
-        result = await conn.execute(text("SELECT version()"))
-        print(result.fetchone())
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-asyncio.run(func())
+session = async_session_maker()
+
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/models/hotels.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+
+
+
+
+class HotelsOrm(Base):
+    __tablename__ = "hotels"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    location: Mapped[str]
