5e1217a8c9b9e9d0599b2405352a75c511103386
Igor39-dev <19gorokhov90@gmail.com>
2026-05-02 23:06:52 +0200

    update and delete data through repo

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -55,15 +55,10 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
 
 
 @router.put("/{hotel_id}")
-def edit_hotel(
-    hotel_id: int,
-    title: str = Body(),
-    name: str = Body(),
-):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    hotel["title"] = title
-    hotel["name"] = name
+async def edit_hotel(hotel_id: int, hotel_data: Hotel):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
 
 
@@ -87,7 +82,8 @@ def partially_edit_hotel(
 
 
 @router.delete("/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+async def delete_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).delete(id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
