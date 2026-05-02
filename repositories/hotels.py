1ff20d03642b92ea0212a42d285eb400bfe96ebb
Igor39-dev <19gorokhov90@gmail.com>
2026-05-02 21:10:59 +0200

    relocate get_hotels method into repository

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -1,6 +1,30 @@
+from sqlalchemy import func, select
 from repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
 
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
+
+    async def get_all(
+        self,
+        location: str | None = None,
+        title: str | None = None,
+        limit: int = 5,
+        offset: int = 0,
+    ):
+        query = select(HotelsOrm)
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+        if title:
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+        print(query.compile(compile_kwargs={"literal_binds": True}))
+
+        result = await self.session.execute(query)
+        return result.scalars().all()
+        
