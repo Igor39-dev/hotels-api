d12a05e9f59f9e26244ccca0ab55d6d8d1d6dbed
Igor39-dev <19gorokhov90@gmail.com>
2026-05-04 23:05:21 +0200

    add access_token into Cookies
    - add schema: UserWithHashedPassword (for using in verify password)
    - update repository: add method get_user_with_hashed_password
    - update @router.post("/login"): password verify using hash-token

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from pwdlib import PasswordHash
 import jwt
 from jwt.exceptions import InvalidTokenError
@@ -15,6 +15,10 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+password_hash = PasswordHash.recommended()
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
@@ -28,7 +32,6 @@ def create_access_token(data: dict) -> str:
 async def register_user(
     data: UserRequestAdd,
 ):
-    password_hash = PasswordHash.recommended()
     hashed_password = password_hash.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
@@ -41,11 +44,14 @@ async def register_user(
 @router.post("/login")
 async def login_user(
     data: UserRequestAdd,
+    response: Response,
 ):
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким email не найден")
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Неверный пароль")
         access_token = create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
-        
