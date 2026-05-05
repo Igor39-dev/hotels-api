9f3d666bb7ab7f7c1c1087fe5d0218a262344b66
Igor39-dev <19gorokhov90@gmail.com>
2026-05-05 03:00:52 +0200

    get user's cookie
    get access token inside endpoint ("/only_auth")

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Response, Request
 from jwt.exceptions import InvalidTokenError
 
 from src.repositories.users import UsersRepository
@@ -37,3 +37,9 @@ async def login_user(
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/only_auth")
+async def only_auth(request: Request):
+    access_token = request.cookies.get("access_token")
+    return access_token
