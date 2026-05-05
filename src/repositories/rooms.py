82d2164582c5827a90fcc29ff0c8fd587a9fbcf4
Igor39-dev <19gorokhov90@gmail.com>
2026-05-05 21:47:31 +0200

    logics/endpoints for rooms
    get("/{hotel_id}/rooms")
    get("/{hotel_id}/rooms/{room_id}")
    post("/{hotel_id}/rooms")
    put("/{hotel_id}/rooms/{room_id}")
    patch("/{hotel_id}/rooms/{room_id}")
    delete("/{hotel_id}/rooms/{room_id}")
    
    modify repositories/base.py: get_all()

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,6 +1,21 @@
+from sqlalchemy import select
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.shemas.rooms import Room, RoomAdd
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
+    schema = Room
+
+    async def get_all(
+        self,
+        hotel_id: int
+    ) -> list[Room]:
+        query = select(RoomsOrm)
+        query = query.filter(RoomsOrm.hotel_id == hotel_id)
+
+        print(query.compile(compile_kwargs={"literal_binds": True}))
+
+        result = await self.session.execute(query)
+        return [Room.model_validate(room, from_attributes=True) for room in result.scalars().all()]
