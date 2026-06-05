b1f3958c464a1730a74005883d9868c0634a1967
Igor39-dev <19gorokhov90@gmail.com>
2026-06-05 16:42:05 +0200

    103 change rooms facilities from api

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -39,6 +39,7 @@ async def create_room(db: DBDep, hotel_id: int, room_data: RoomAddRequest = Body
 async def edit_room(db: DBDep, hotel_id: int, room_id: int, room_data: RoomAddRequest):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     await db.rooms.edit(_room_data, id=room_id)
+    await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=room_data.facilities_ids)
     await db.commit()
     return {"status": "OK"}
 
@@ -50,8 +51,11 @@ async def partially_edit_room(
     room_id: int,
     room_data: RoomPatchRequest
 ):
-    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
+    _room_data_dict = room_data.model_dump(exclude_unset=True)
+    _room_data = RoomPatch(hotel_id=hotel_id, **_room_data_dict)
     await db.rooms.edit(_room_data, is_patch=True, id=room_id, hotel_id=hotel_id)
+    if "facilities_ids" in _room_data_dict:
+        await db.rooms_facilities.set_room_facilities(room_id, facilities_ids=_room_data_dict["facilities_ids"])
     await db.commit()
     return {"status": "OK"}
 
