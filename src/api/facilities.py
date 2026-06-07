79b719915fee14f1b061a9952f23572b0ad24efd
Igor39-dev <19gorokhov90@gmail.com>
2026-06-07 02:13:22 +0200

    118 cache in routers
    Using redis for caching facilities

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,7 +1,13 @@
+import json
+from src.init import redis_manager
+
 from datetime import date
+
 from fastapi import APIRouter, Body, Query
 from fastapi.openapi.models import Example
 from src.api.dependencies import DBDep
+
+from src.shemas import facilities
 from src.shemas.facilities import FacilityAdd
 from src.shemas.rooms import RoomAdd, RoomAddRequest, RoomPatch, RoomPatchRequest
 
@@ -10,11 +16,22 @@ router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 
 @router.get("")
-async def get_facilities(
-        db: DBDep,
-):
-    return await db.facilities.get_all()
+async def get_facilities(db: DBDep):
+    facilities_from_cache = await redis_manager.get("facilities")
+    print(f"{facilities_from_cache=}")
+
+    if not facilities_from_cache:
+        print("!!!No facilities in cache!!!")
+        facilities = await db.facilities.get_all()
+        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set("facilities", facilities_json, 10)
+        return facilities
     
+    else:
+        facilities_dicts = json.loads(facilities_from_cache)
+        return facilities_dicts
+
 
 
 @router.post("")
--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
+
 from src.api.hotels import router as router_hotels
 from src.api.auth import router as router_auth
 from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
+from src.init import redis_manager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
 
 app = FastAPI(
     title="Hotels API",
     description="API для работы с отелями",
+    lifespan=lifespan
 )
 
 app.include_router(router_auth)
