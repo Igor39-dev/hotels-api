087be887ad883e2cafd387c11b6e79561034f46e
Igor39-dev <19gorokhov90@gmail.com>
2026-07-11 17:19:39 +0200

    139 work with DB and API in fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,12 @@ from src.utlils.db_manager import DBManager
 def check_mode():
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="function", autouse=True)
+async def db() -> DBManager:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
   
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database(check_mode):
@@ -30,19 +36,23 @@ async def setup_database(check_mode):
     hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
     rooms = [RoomAdd.model_validate(room) for room in rooms]
 
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        await db.hotels.add_bulk(hotels)
-        await db.rooms.add_bulk(rooms)
-        await db.commit()
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def register_user(setup_database):
+async def ac() -> AsyncClient:
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        await ac.post(
-            "/auth/register",
-            json={
-                "email": "test@test.com",
-                "password": "test",
-            },
-        )
+        yield ac
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(ac, setup_database):
+    await ac.post(
+        "/auth/register",
+        json={
+            "email": "test@test.com",
+            "password": "test",
+        },
+    )
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -3,9 +3,13 @@ from src.utlils.db_manager import DBManager
 from src.database import async_session_maker_null_pool
 
 
-async def test_add_hotel():
+async def test_add_hotel(db):
     hotel_data = HotelAdd(title="Test Hotel", location="Test Location")
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        new_hotel_data = await db.hotels.add(hotel_data)
-        await db.commit()
+    new_hotel_data = await db.hotels.add(hotel_data)
+    await db.commit()
 
+
+async def test_add_hotel2(db):
+    hotel_data = HotelAdd(title="Test Hotel 01", location="Test Location")
+    new_hotel_data = await db.hotels.add(hotel_data)
+    await db.commit()
