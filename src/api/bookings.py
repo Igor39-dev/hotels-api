0408bec6878be868e70427f8238abb02766e7faf
Igor39-dev <19gorokhov90@gmail.com>
2026-07-24 01:52:56 +0200

    160 handling of exceptions

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from src.api.dependencies import DBDep, UserIdDep
+from src.exceptions import ObjectNotFoundException
 from src.shemas.bookings import BookingAdd, BookingAddRequest
+from src.shemas.hotels import Hotel
+from src.shemas.rooms import Room
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
@@ -17,9 +20,15 @@ async def get_bookings_me(user_id: UserIdDep, db: DBDep):
 
 
 @router.post("")
-async def add_booking(user_id: UserIdDep, db: DBDep, booking_data: BookingAddRequest):
-    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
-    hotel = await db.hotels.get_one_or_none(id=room.hotel_id)
+async def add_booking(user_id: UserIdDep,
+    db: DBDep,
+    booking_data: BookingAddRequest
+):
+    try:
+        room: Room = await db.rooms.get_one(id=booking_data.room_id)
+    except ObjectNotFoundException:
+        raise HTTPException(status_code=400, detail="Номер не найден")
+    hotel: Hotel = await db.hotels.get_one(id=room.hotel_id)
     room_price = room.price
     _booking_data = BookingAdd(
         user_id=user_id,
---
+++ src/exceptions.py
@@ -0,0 +1,10 @@
+
+
+class NabronirovalException(Exception):
+    detail = "Неожиданная ошибка"
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+class ObjectNotFoundException(NabronirovalException):
+    detail = "Объект не найден"
